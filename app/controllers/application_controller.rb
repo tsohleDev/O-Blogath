a036d1dbe389bee1440974c6f839463e538604d0
tsohleDev <mokhemisitsohle@gmail.com>
2023-04-15 15:25:28 +0200

    Forms
    create form for new post and new comment
    liking action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    # set the current user to the first user in the database
+    @current_user ||= User.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 
   root 'users#index'
 
   get '/error', to: 'users#error'
+
+  post '/users/:user_id/posts/:post_id/comments', to: 'comments#create', as: 'create_post_comment'
+  get '/users/:user_id/posts/:id/like', to: 'posts#like', as: 'like_post'
+  get '/users/:user_id/posts/:id/unlike', to: 'posts#unlike', as: 'unlike_post'
 end
