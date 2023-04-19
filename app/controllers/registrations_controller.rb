3af266fc6e8f2ca354ed8e1cf7545e7504ef8b54
Allassane Dicko <dickoallassane.dev@gmail.com>
2023-04-19 14:37:28 +0100

    Add registration_controller.rb

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController
+    before_action :configure_sign_up_params, only: [:create]
+    before_action :configure_account_update_params, only: [:update]
+    private
+    def configure_sign_up_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :picture_url])
+    end
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :picture_url])
+    end
+end
